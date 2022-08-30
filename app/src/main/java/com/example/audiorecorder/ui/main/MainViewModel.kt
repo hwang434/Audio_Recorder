24eda7d887cec1f505f1218aaa34e0255b05d20c
hwang434 <inqbator@knou.ac.kr>
2022-08-30 12:57:17 +0900

    오디오 파일 직접 설정해서 올리기 성공

--- app/src/main/java/com/example/audiorecorder/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/audiorecorder/ui/main/MainViewModel.kt
@@ -1,20 +1,18 @@
 package com.example.audiorecorder.ui.main
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.audiorecorder.ui.main.domain.IStorageDao
 import com.example.audiorecorder.ui.main.domain.StorageDao
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     // Get the default bucket from a custom FirebaseApp
     private val storageReference: IStorageDao by lazy { StorageDao() }
 
-    fun uploadVoice() {
-        viewModelScope.launch(Dispatchers.IO) {
-            storageReference.uploadVoice()
-        }
+    suspend fun uploadVoice(uri: Uri) {
+        delay(3000)
+        storageReference.uploadVoice(uri)
     }
 }
