4445c7d7462d026cbaad940f13c7a82745063684
hwang434 <inqbator@knou.ac.kr>
2022-09-02 15:40:34 +0900

    음성 녹음 UI 변경을 위한 뷰모델 메소드 추가

--- app/src/main/java/com/example/audiorecorder/viewmodels/VoiceViewModel.kt
+++ app/src/main/java/com/example/audiorecorder/viewmodels/VoiceViewModel.kt
@@ -9,9 +9,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.audiorecorder.domain.IStorageDao
 import com.example.audiorecorder.domain.StorageDao
 import com.example.audiorecorder.dto.Voice
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.example.audiorecorder.utils.Resource
+import kotlinx.coroutines.*
 
 class VoiceViewModel : ViewModel() {
 
@@ -39,6 +38,7 @@ class VoiceViewModel : ViewModel() {
 
     // uri 에 위치한 음성을 업로드함.
     suspend fun uploadVoice(uri: Uri) {
+        Log.d(TAG,"VoiceViewModel - uploadVoice() called")
         delay(3000)
         storageReference.uploadVoice(uri)
     }
@@ -57,5 +57,38 @@ class VoiceViewModel : ViewModel() {
     suspend fun getDownloadLinkOfVoice(uri: Uri): Uri {
         Log.d(TAG,"VoiceViewModel - getDownloadLinkOfVoice() called")
         return storageReference.getDownloadLinkOfVoice(uri)
+    }
+
+    fun startTimer(time: Long) {
+        Log.d(TAG,"VoiceViewModel - startTimer() called")
+        changeRecordingStatus()
+        updateStartTime(time)
+    }
+
+    // 녹음 상태를 반대로 변경함.
+    private fun changeRecordingStatus() {
+        Log.d(TAG,"VoiceViewModel - changeRecordingStatus() called")
+        _isRecording.value = !(_isRecording.value!!)
+        Log.d(TAG,"VoiceViewModel - _isRecording.value : ${_isRecording.value}() called")
+    }
+
+    // 음성 녹음을 시작한 시각을 계속 post해서 UI를 업데이트함.
+    private fun updateStartTime(time: Long) {
+        Log.d(TAG,"VoiceViewModel - updateStartTime() called")
+
+        viewModelScope.launch(Dispatchers.IO) {
+            // 녹음 중인 동안에만 UI를 업데이트함.
+            while (_isRecording.value == true) {
+                yield()
+                _statTime.postValue(Resource.Success(time))
+                delay(1000)
+            }
+        }
+    }
+
+    fun stopTimer() {
+        Log.d(TAG,"VoiceViewModel - stopTimer()  isRecoring.value : ${_isRecording.value}called")
+        _isRecording.value = false
+        _statTime.value = Resource.Success(0)
     }
 }
