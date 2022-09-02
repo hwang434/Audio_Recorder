93efa704da474bc9f57d82ff4bc1d955158fae05
hwang434 <inqbator@knou.ac.kr>
2022-09-02 15:43:11 +0900

    녹음 시작 시 UI 설정을 위한 메소드 실행.

--- app/src/main/java/com/example/audiorecorder/views/MainFragment.kt
+++ app/src/main/java/com/example/audiorecorder/views/MainFragment.kt
@@ -2,15 +2,12 @@ package com.example.audiorecorder.views
 
 import android.Manifest
 import android.app.AlertDialog
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
@@ -21,14 +18,15 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
 import com.example.audiorecorder.R
 import com.example.audiorecorder.RecordService
 import com.example.audiorecorder.databinding.FragmentMainBinding
+import com.example.audiorecorder.utils.TimerDateFormat
 import com.example.audiorecorder.viewmodels.VoiceViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -56,6 +54,7 @@ class MainFragment : Fragment() {
         setDirectoryOfVoice()
         // 파일을 찾을 인텐트 실행
         registerIntentForPickAudio()
+        setObserver()
     }
 
     override fun onCreateView(
@@ -64,9 +63,65 @@ class MainFragment : Fragment() {
     ): View {
         Log.d(TAG,"MainFragment - onCreateView() called")
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        initButton()
         setEvent()
 
         return binding.root
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"MainFragment - onDestroy() called")
+        super.onDestroy()
+        requireActivity().stopService(recordService)
+    }
+
+    // 음성을 저장할 폴더 설정. 현재 다운로드 폴더로 되어 있음.
+    private fun setDirectoryOfVoice() {
+        directoryOfVoice = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
+    }
+
+    // 오디오 파일 URI 조회 및 업로드를 위해 시작할 인텐트 초기화.
+    private fun registerIntentForPickAudio() {
+        Log.d(TAG,"MainFragment - registerIntent() called")
+        intentOfPickAudio = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            Log.d(TAG,"MainFragment - uri : $uri")
+            uri?.let { it ->
+                uploadVoice(it)
+            }
+        }
+    }
+
+    private fun setObserver() {
+        voiceViewModel.startTime.observe(this) { startTime ->
+            Log.d(TAG,"MainFragment - setObserver() startTime : ${startTime.data} called")
+            val time = if (startTime.data == 0L) {
+                TimerDateFormat.makeLongToTimerFormat(0)
+            } else {
+                TimerDateFormat.makeLongToTimerFormat(System.currentTimeMillis() - startTime.data!!)
+            }
+
+            binding.textviewOngoingTime.text = time
+        }
+
+        voiceViewModel.isRecording.observe(this) { isRecording ->
+            // if : 녹음 중이면 녹음 버튼 보여주기.
+            // else : 녹음 중이 아니면 정지 버튼 숨기기
+            if (isRecording) {
+                binding.buttonRecordVoiceButton.visibility = View.GONE
+                binding.buttonStopRecord.visibility = View.VISIBLE
+            } else {
+                binding.buttonRecordVoiceButton.visibility = View.VISIBLE
+                binding.buttonStopRecord.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun initButton() {
+        // if : 뷰를 다시 그릴 때 만약 녹음 중이면, 녹음 버튼 없애고, 중지 버튼 그림.
+        if (voiceViewModel.isRecording.value == true) {
+            binding.buttonRecordVoiceButton.visibility = View.GONE
+            binding.buttonStopRecord.visibility = View.VISIBLE
+        }
     }
 
     private fun setEvent() {
@@ -117,8 +172,22 @@ class MainFragment : Fragment() {
     // 녹음 시작
     private fun startRecord() {
         Log.d(TAG,"MainFragment - startRecord() called")
-        originalName = "${System.currentTimeMillis()}.mp3"
+        val startTime = System.currentTimeMillis()
+        originalName = "$startTime.mp3"
+        // 녹음 서비스 실행
         startRecordService()
+        // 녹음 시작 시각 업데이트 및 상태 변경
+        startViewModelTimer(System.currentTimeMillis())
+    }
+
+    private fun startViewModelTimer(time: Long) {
+        Log.d(TAG,"MainFragment - updateStartTime() called")
+        voiceViewModel.startTimer(time)
+    }
+    
+    private fun stopViewModelTimer() {
+        Log.d(TAG,"MainFragment - stopViewModelTimer() called")
+        voiceViewModel.stopTimer()
     }
 
     // 녹음 서비스 실행
@@ -136,6 +205,10 @@ class MainFragment : Fragment() {
     // 정지 = 아예 정지하고 파일을 저장함.
     private fun stopRecord() {
         Log.d(TAG,"MainFragment - stopRecord() called")
+        // 뷰모델의 시각을 0으로 바꾸고 isRecording을 false로 바꿈.
+        stopViewModelTimer()
+        createFileNameAlert()?.apply { show() }
+    }
 
     // 녹음 파일 제목을 정해주는 alertDialog 리턴
     private fun createFileNameAlert(): AlertDialog? {
@@ -192,17 +265,6 @@ class MainFragment : Fragment() {
         } catch (e: Exception) {
             Log.w(TAG, "uploadVoice: ", e)
             Toast.makeText(requireContext(), "오디오 파일 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    // 오디오 파일 URI 조회 및 업로드를 위해 시작할 인텐트 초기화.
-    private fun registerIntentForPickAudio() {
-        Log.d(TAG,"MainFragment - registerIntent() called")
-        intentOfPickAudio = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            Log.d(TAG,"MainFragment - uri : $uri")
-            uri?.let { it ->
-                uploadVoice(it)
-            }
         }
     }
 }
